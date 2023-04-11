e0b8bb7bc0f2e2c15d7cfcb47b0cc3e5a098c180
zekeriya <zekeriyaunutmaz@gmail.com>
2023-04-11 22:44:14 +0300

    Forget_Pass

---
+++ lib/pages/auth/forget_pass.dart
@@ -0,0 +1,136 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:whoami/service/auth.service.dart';
+import 'package:whoami/utils/customColors.dart';
+import 'package:whoami/utils/customTextStyle.dart';
+import 'package:whoami/widgets/custom_text_button.dart';
+
+class ForgetPassword extends StatefulWidget {
+  const ForgetPassword({Key? key}) : super(key: key);
+
+  @override
+  State<ForgetPassword> createState() => _ForgetPasswordState();
+}
+
+class _ForgetPasswordState extends State<ForgetPassword> {
+  TextEditingController forgetPasswordController = TextEditingController();
+  final formkey = GlobalKey<FormState>();
+  final firebaseAuth = FirebaseAuth.instance;
+  final authService = AuthService();
+  @override
+  Widget build(BuildContext context) {
+    var height = MediaQuery.of(context).size.height;
+    String topImage = "assets/images/topImage.png";
+    return Scaffold(
+      body: appBody(height, topImage),
+    );
+  }
+
+  SingleChildScrollView appBody(double height, String topImage) {
+    return SingleChildScrollView(
+      child: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            topImageContainer(height, topImage),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Form(
+                key: formkey,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    titleText(),
+                    customSizedBox(),
+                    emailTextField(),
+                    customSizedBox(),
+                    forgotPasswordButton(),
+                    customSizedBox(),
+                    customSizedBox(),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Text titleText() {
+    return const Text(
+      "Lutfen  E-mailiniz giriniz",
+      style: CustomTextStyle.titleTextStyle,
+    );
+  }
+
+  TextFormField emailTextField() {
+    return TextFormField(
+      controller: forgetPasswordController,
+      style: const TextStyle(color: Colors.white),
+      decoration: customInputDecoration("Email"),
+    );
+  }
+
+  Center forgotPasswordButton() {
+    return Center(
+      child: TextButton(
+        onPressed: () async {
+          var forgetEmail = forgetPasswordController.text.trim();
+          try {
+            FirebaseAuth.instance.sendPasswordResetEmail(email: forgetEmail);
+            ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(
+                content: Text("Lutfen Emailinizi Kontrol ediniz..."),
+              ),
+            );
+          } on FirebaseAuthException catch (e) {
+            print('Erro $e');
+          }
+        },
+        child: customText(
+          "Dogrulama Emaili Yolla ",
+          CustomColors.textButtonColor,
+        ),
+      ),
+    );
+  }
+
+  Container topImageContainer(double height, String topImage) {
+    return Container(
+      height: height * .25,
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          fit: BoxFit.cover,
+          image: AssetImage(topImage),
+        ),
+      ),
+    );
+  }
+
+  Widget customSizedBox() => const SizedBox(
+        height: 20,
+      );
+
+  Widget customText(String text, Color color) => Text(
+        text,
+        style: TextStyle(color: color),
+      );
+
+  InputDecoration customInputDecoration(String hintText) {
+    return InputDecoration(
+      hintText: hintText,
+      hintStyle: const TextStyle(color: Colors.grey),
+      enabledBorder: const UnderlineInputBorder(
+        borderSide: BorderSide(
+          color: Colors.grey,
+        ),
+      ),
+      focusedBorder: const UnderlineInputBorder(
+        borderSide: BorderSide(
+          color: Colors.grey,
+        ),
+      ),
+    );
+  }
+}
