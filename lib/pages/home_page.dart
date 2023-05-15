84c25754eeaf2924fc96a7b10b274b8195e96c70
zekeriya <zekeriyaunutmaz@gmail.com>
2023-05-15 21:40:07 +0300

    SearchBar

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: avoid_print
+
 import 'package:flutter/material.dart';
 import 'package:whoami/widgets/NavBar.dart';
 
@@ -12,32 +14,12 @@ class _HomePageState extends State<HomePage> {
   final List<Map> myProducts =
       List.generate(100000, (index) => {"id": index, "name": "Product $index"})
           .toList();
-
+  final myController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: searchBar(),
       drawer: NavBar(),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        // implement GridView.builder
-        child: GridView.builder(
-            gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-                maxCrossAxisExtent: 200,
-                childAspectRatio: 3 / 2,
-                crossAxisSpacing: 20,
-                mainAxisSpacing: 20),
-            itemCount: myProducts.length,
-            itemBuilder: (BuildContext ctx, index) {
-              return Container(
-                alignment: Alignment.center,
-                decoration: BoxDecoration(
-                    color: Colors.amber,
-                    borderRadius: BorderRadius.circular(15)),
-                child: Text(myProducts[index]["name"]),
-              );
-            }),
-      ),
     );
   }
 
@@ -51,6 +33,7 @@ class _HomePageState extends State<HomePage> {
           color: Colors.white, borderRadius: BorderRadius.circular(5)),
       child: Center(
         child: TextField(
+          controller: myController,
           decoration: InputDecoration(
               // prefixIcon: const Icon(Icons.search),
               suffixIcon: IconButton(
@@ -58,7 +41,7 @@ class _HomePageState extends State<HomePage> {
                 onPressed: () {
                   /* Clear the search field */
 
-                  print('Arama başarılı');
+                  print(myController.text);
                 },
               ),
               hintText: '  Aramak İstediğiniz Kullanıcı adını giriniz ',
