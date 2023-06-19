6fd733ee3ad064e0753f22ec7eae83440abbb55b
zekeriya <zekeriyaunutmaz@gmail.com>
2023-06-19 21:51:26 +0300

    Launcher Icon

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'UserSearch',
       routes: {
         "/loginPage": (context) => const LoginPage(),
         "/signUp": (context) => SignUp(),
